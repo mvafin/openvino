b3084c2a60a08ee747a5810081fa20cfea5f8b95
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-09-27 08:35:24 +0300

    ngraph codestyle

--- ngraph/frontend/tensorflow/src/op/elu.cpp
+++ ngraph/frontend/tensorflow/src/op/elu.cpp
@@ -12,12 +12,12 @@ using namespace ngraph;
 using namespace ngraph::frontend::tensorflow::detail;
 
 namespace tensorflow {
-    namespace ngraph_bridge {
+namespace ngraph_bridge {
 
-        OutputVector TranslateEluOp(const NodeContext& node) {
-            auto input = node.get_ng_input(0);
-            auto alpha = 1.0; // node.get_attribute<float>("alpha");
-            return {ConstructNgNode<opset::Elu>(node.get_name(), input, alpha)};
-        }
-    }  // namespace ngraph_bridge
+OutputVector TranslateEluOp(const NodeContext& node) {
+    auto input = node.get_ng_input(0);
+    auto alpha = 1.0;  // node.get_attribute<float>("alpha");
+    return {ConstructNgNode<opset::Elu>(node.get_name(), input, alpha)};
+}
+}  // namespace ngraph_bridge
 }  // namespace tensorflow
