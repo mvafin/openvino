0528e6814b337ccad18dd50a9ee1edc5b68bd761
Maxim Vafin <maxim.vafin@intel.com>
2022-08-23 15:27:31 +0200

    Add more types

--- src/frontends/tensorflow/src/node_context.cpp
+++ src/frontends/tensorflow/src/node_context.cpp
@@ -9,9 +9,20 @@ using namespace ov::frontend::tensorflow;
  ov::Any NodeContext::apply_additional_conversion_rules(const ov::Any& data,
                                                        const std::type_info& type_info) const override {
     if (data.is<EmptyList>(){
-        if (type_info == typeid(std::vector<int64_t>)) {
+        switch (type_info) {
+        case typeid(std::vector<int64_t>):
             return std::vector<int64_t>();
-        } else {
+        case typeid(std::vector<float>):
+            return std::vector<float>();
+        case typeid(std::vector<std::string>):
+            return std::vector<std::string>();
+        case typeid(std::vector<bool>):
+            return std::vector<bool>();
+        case typeid(std::vector<ov::PartialShape>):
+            return std::vector<ov::PartialShape>();
+        case typeid(std::vector<ov::element::Type>):
+            return std::vector<ov::element::Type>();
+        default:
             FRONT_END_GENERAL_CHECK(false,
                                     "Could not decode empty list attribute for ",
                                     get_name(),
